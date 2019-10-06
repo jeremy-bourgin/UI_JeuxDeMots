3a99fa08da9b1ef92576f47350b6f63053a8d9aa
Abdallah Herri <abdallah.herri@etu.umontpellier.fr>
2019-10-06 18:55:10 +0200

    cache limit

--- Server/functions/cache_manager.php
+++ Server/functions/cache_manager.php
@@ -11,6 +11,7 @@ function has_cache($word)
 
 function save_cache($word, $data)
 {
+	check_and_delete();
 	return file_put_contents(word_to_filename($word), $data);
 }
 
@@ -20,6 +21,23 @@ function retrieve_cache ($word)
 	$result = file_get_contents($filename);
 
 	return $result;
+}
+
+function check_and_delete()
+{
+	$files = glob(CACHE_DIRECTORY."\*");
+	$exclude_files = array('.', '..');
+
+	if (!in_array($files, $exclude_files)) {
+		array_multisort(
+			array_map('filemtime',$files),
+			SORT_ASC,
+			$files
+		);
+	}
+
+	if(count($files) >= LIMIT_NB_CACHE_FILE)
+		unlink($files[0]);
 }
 
 ?>
