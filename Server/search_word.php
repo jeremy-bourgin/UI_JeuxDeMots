f2164ee3b345ed059488840b4197f9c15123cb3d
jbourgin <jeremy.bourgin@etu.umontpellier.fr>
2019-10-06 21:38:57 +0200

    benchmark

--- Server/search_word.php
+++ Server/search_word.php
@@ -9,6 +9,8 @@ function load()
 	$data = request($_GET[PARAMETER_TERM]);
 	$filters = array();
 	
+	$bench_filter = Benchmark::startBench("filter");
+	
 	if (isset($_GET[PARAMETER_NOT_OUT]))
 	{
 		$filters[] = new FilterOut();
@@ -27,6 +29,8 @@ function load()
 	$filters[] = new FilterLimit($_GET[PARAMETER_PAGE], LIMIT_NB_WORD);
 	
 	Word::filterRelations($data, $filters);
+	
+	$bench_filter->end();
 	
 	return $data;
 }
