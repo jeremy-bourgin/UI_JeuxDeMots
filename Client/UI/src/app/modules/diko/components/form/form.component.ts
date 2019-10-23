f97f51d1ea49bce09d14fc7d33d0030f5a293d8b
jbourgin <jeremy.bourgin@etu.umontpellier.fr>
2019-10-23 22:46:40 +0200

    form

--- Client/UI/src/app/modules/diko/components/form/form.component.ts
+++ Client/UI/src/app/modules/diko/components/form/form.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SearchService } from '../../services/search.service';
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
+
 export class FormComponent implements OnInit {
 
-  constructor() { }
+	private term : string;
 
-  ngOnInit() {
-  }
+	private node : string;
+
+	private not_out : string;
+
+	private not_in : string;
+
+	private nb_terms : string;
+
+	constructor(private search_service : SearchService) { }
+
+	ngOnInit()
+	{
+	}
+	
+	onSubmit()
+	{
+		console.log(this.term);
+	}
 
 }
