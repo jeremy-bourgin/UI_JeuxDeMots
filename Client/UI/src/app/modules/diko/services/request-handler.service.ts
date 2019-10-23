91fa54d5314457d1b98a47bc2456b98243e090e6
jbourgin <jeremy.bourgin@etu.umontpellier.fr>
2019-10-24 00:20:41 +0200

    request handler begin

--- Client/UI/src/app/modules/diko/services/request-handler.service.ts
+++ Client/UI/src/app/modules/diko/services/request-handler.service.ts
@@ -1,9 +1,59 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RequestHandlerService {
 
-  constructor() { }
+export class RequestHandlerService
+{
+	static readonly url: string = "http://lafourcade/Server/";
+	
+	static readonly services: any = {
+		"search_word": "search_word.php"
+	};
+	
+	constructor(private http: HttpClient)
+	{
+		
+	}
+	
+	public request(service_name : string, params : any, success_callback : Function): void
+	{
+		var o = makeInformations(service_name, params);
+		var http_get: Observable<any> = this.http.get(o.action, {headers: o.headers});
+	}
+	
+	private makeInformations(service_name : string, params: any): any {
+		/* begin : header */
+		var temp: any = {};
+
+		temp["Accept"] = "application/json";
+		temp["Content-Type"] = "application/json";
+
+		var headers : HttpHeaders = new HttpHeaders(temp);
+		/* end : header */
+
+		/* begin : service url */
+		var action : string = RequestHandlerService.url + RequestHandlerService.services[service_name];
+
+		// url parameters
+		var sign : string = "?";
+		
+		for (var p in params)
+		{
+			action += sign + p + "=" + params[p];
+			sign = "&";
+		}
+		/* end : service url */
+
+		var result: any = {
+			action: action,
+			headers: headers,
+		};
+
+		return result;
+	}
+
 }
