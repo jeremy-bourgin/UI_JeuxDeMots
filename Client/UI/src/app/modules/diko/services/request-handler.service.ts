f8bba3d66b513878319ad002257d6b35d3d0c3a3
Bourgin Jeremy <jeremy.bourgin@etu.umontpellier.fr>
2019-11-08 11:40:35 +0100

    bootstrap

--- Client/UI/src/app/modules/diko/services/request-handler.service.ts
+++ Client/UI/src/app/modules/diko/services/request-handler.service.ts
@@ -9,21 +9,21 @@ import { Observable } from 'rxjs';
 export class RequestHandlerService
 {
 	public static readonly url: string = "http://lafourcade/Server/";
-	
+
 	public static readonly services: any = {
 		search_word: "search_word.php"
 	};
-	
+
 	constructor(private http: HttpClient)
 	{
-		
+
 	}
-	
+
 	public request(service_name : string, params : any, callback : Function): void
 	{
 		var self: RequestHandlerService = this;
 		var o : any = this.makeInformations(service_name, params);
-		var service : Observable<any> = this.http.get(o.action, {headers: o.headers});		
+		var service : Observable<any> = this.http.get(o.action, {headers: o.headers});
 
 		function action_callback(data: any): void {
 			if (data.error) {
@@ -43,7 +43,7 @@ export class RequestHandlerService
 		service.subscribe(action_callback);
 
 	}
-	
+
 	private makeInformations(service_name : string, params: any): any {
 		/* begin : header */
 		var temp: any = {
@@ -58,7 +58,7 @@ export class RequestHandlerService
 
 		// url parameters
 		var sign : string = "?";
-		
+
 		for (var p in params)
 		{
 			action += sign + p + "=" + params[p];
