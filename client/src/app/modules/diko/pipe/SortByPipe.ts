fc3ef3a1ab80d7bfd91a07f211ea5ed12dd6a435
jbourgin <jeremy.bourgin@etu.umontpellier.fr>
2019-12-18 15:24:03 +0100

    sort

--- client/src/app/modules/diko/pipe/SortByPipe.ts
+++ client/src/app/modules/diko/pipe/SortByPipe.ts
@@ -4,11 +4,36 @@ import * as _ from 'lodash';
 @Pipe({ name: 'sortBy' })
 export class SortByPipe implements PipeTransform {
 
-  transform(value: any[], order = '', column: string = ''): any[] {
+  transform(value: any[], order = 'asc', column: string = ''): any[] {
     if (!value || order === '' || !order) { return value; } // no array
     if (!column || column === '') { return _.sortBy(value); } // sort 1d array
     if (value.length <= 1) { return value; } // array with only one item
-    
-    return _.orderBy(value, [column], [order]);
+
+    function sort(a: any, b: any): number
+    {
+      var temp_a : string = a[column].toString();
+      var temp_b : string = b[column].toString();
+
+      var options: any = {
+        sensitivity: 'base',
+        numeric: false
+      }
+
+      if (isNaN(a[column]) && isNaN(b[column]))
+      {
+        options.numeric = true;
+      }
+
+      return temp_a.localeCompare(temp_b, 'fr', options);
+    }
+
+    var r: any = Array.from(value).sort(sort);
+
+    if (order === 'desc')
+    {
+      r.reverse();
+    }
+
+    return r;
   }
 }
