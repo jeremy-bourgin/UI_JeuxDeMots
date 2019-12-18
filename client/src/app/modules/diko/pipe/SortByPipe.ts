5b2fde561f8c7e49b5ef971f3db843c250d39a45
jbourgin <jeremy.bourgin@etu.umontpellier.fr>
2019-12-18 19:58:22 +0100

    sort

--- client/src/app/modules/diko/pipe/SortByPipe.ts
+++ client/src/app/modules/diko/pipe/SortByPipe.ts
@@ -3,48 +3,47 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'sortBy' })
 export class SortByPipe implements PipeTransform {
 
-  transform(value: any[], order = 'asc', column: string = ''): any[] {
-    if (!value) { return value; } // no array
-    if (value.length <= 1) { return value; } // array with only one item
+	transform(value: any[], order = 'asc', column: string = ''): any[] {
+		if (!value) { return value; } // no array
+		if (value.length <= 1) { return value; } // array with only one item
 
-    function get_value(e: any): any
-    {
-      if (!column || column === '')
-      {
-        return e;
-      }
+		function get_value(e: any): any
+		{
+			if (!column || column === '')
+			{
+				return e;
+			}
 
-      return e[column];
-    }
+			return e[column];
+		}
 
-    function sort(a: any, b: any): number
-    {
-      var temp_a : any = get_value(a);
-      var temp_b : any = get_value(b);
+		function sort(a: any, b: any): number
+		{
+			var temp_a : any = get_value(a);
+			var temp_b : any = get_value(b);
+		
+			if (!isNaN(temp_a) && !isNaN(temp_b))
+			{
+				return (temp_a - temp_b);
+			}
+	  
+			var temp_a_string : string = temp_a.toString();
+			var temp_b_string : string = temp_b.toString();
 
-      var temp_a_string : string = temp_a.toString();
-      var temp_b_string : string = temp_b.toString();
+			var options: any = {
+				sensitivity: 'base'
+			}
 
-      var options: any = {
-        sensitivity: 'base',
-        numeric: false
-      }
+			return temp_a_string.localeCompare(temp_b_string, 'fr', options);
+		}
 
-      if (!isNaN(temp_a) && !isNaN(temp_b))
-      {
-        options.numeric = true;
-      }
+		var r: any = Array.from(value).sort(sort);
 
-      return temp_a_string.localeCompare(temp_b_string, 'fr', options);
-    }
+		if (order === 'desc')
+		{
+			r.reverse();
+		}
 
-    var r: any = Array.from(value).sort(sort);
-
-    if (order === 'desc')
-    {
-      r.reverse();
-    }
-
-    return r;
-  }
+		return r;
+	}
 }
