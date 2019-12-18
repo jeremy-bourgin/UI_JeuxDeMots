e43b6f90aab67b1b1ebf321a6e89f6c2f25a291f
jbourgin <jeremy.bourgin@etu.umontpellier.fr>
2019-12-18 15:30:05 +0100

    sort

--- client/src/app/modules/diko/pipe/SortByPipe.ts
+++ client/src/app/modules/diko/pipe/SortByPipe.ts
@@ -4,26 +4,38 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SortByPipe implements PipeTransform {
 
   transform(value: any[], order = 'asc', column: string = ''): any[] {
-    if (!value || order === '' || !order) { return value; } // no array
-    if (!column || column === '') { return _.sortBy(value); } // sort 1d array
+    if (!value) { return value; } // no array
     if (value.length <= 1) { return value; } // array with only one item
+
+    function get_value(e: any): any
+    {
+      if (!column || column === '')
+      {
+        return e;
+      }
+
+      return e[column];
+    }
 
     function sort(a: any, b: any): number
     {
-      var temp_a : string = a[column].toString();
-      var temp_b : string = b[column].toString();
+      var temp_a : any = get_value(a);
+      var temp_b : any = get_value(b);
+
+      var temp_a_string : string = temp_a.toString();
+      var temp_b_string : string = temp_b.toString();
 
       var options: any = {
         sensitivity: 'base',
         numeric: false
       }
 
-      if (!isNaN(a[column]) && !isNaN(b[column]))
+      if (!isNaN(temp_a) && !isNaN(temp_b))
       {
         options.numeric = true;
       }
 
-      return temp_a.localeCompare(temp_b, 'fr', options);
+      return temp_a_string.localeCompare(temp_b_string, 'fr', options);
     }
 
     var r: any = Array.from(value).sort(sort);
