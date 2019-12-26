1bbe143d5cb3f630ce6d2547a3533488de730e2c
jbourgin <jeremy.bourgin@etu.umontpellier.fr>
2019-12-26 18:44:35 +0100

    prod mode + bug

--- deploy.php
+++ deploy.php
@@ -8,7 +8,7 @@ $api_dest_const = $dest_dest_dir . "/functions/constante.php";
 $gitkeep = __DIR__ .  "/public/.gitkeep";
 
 $const = array(
-	"PROD_MODE" => false
+	"PROD_MODE" => true
 );
 
 function xcopy($src, $dst)
